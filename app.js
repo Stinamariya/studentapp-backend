04d6e07148c4f8afc09e30b0d015c1a14a9ccb9b
Stinamariya <stinamariya2006@gmail.com>
2024-06-11 14:07:28 +0530

    complete backend

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require("express")
 const mongoose= require("mongoose")
 const cors= require("cors")
-
+const {studentmodel}=require("./models/student")
 const app=express()
 app.use(cors())
+app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+mongoose.connect("mongodb+srv://stina:stina2006@cluster0.rfrzosg.mongodb.net/studentdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add",(req,res)=>{
+    let input=req.body
+    let student=new studentmodel(input)
+    student.save()
+    res.json({status:"success"})
 })
-app.get("/contact",(req,res)=>{
-    res.send("welcome to my contact page")
+
+app.get("/view",(req,res)=>{
+    studentmodel.find().then(
+        (data)=>{
+            res.json(data)
+            
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
 })
 
-
-app.listen(8080,()=>{
+app.listen(8081,()=>{
     console.log("server started")
 })
---
+++ models/student.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema(
+    {
+        "name":{type:String,required:true},
+        "rollNo":{type:String,required:true},
+        "admNO":{type:String,required:true},
+        "college":{type:String,required:true}
+    }
+)
+
+let studentmodel=mongoose.model("students",schema);
+module.exports={studentmodel}
